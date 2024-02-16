bb20e9b76c7e48a4f6ec070d5f3e3761390abfa0
its-ankur <ankuratul2007@gmail.com>
2024-02-16 12:15:36 +0530

    dynamic 2d array

---
+++ c++/dynamic2DArray.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int **a=new int*[n];
+    int *arr=new int[n*m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>*(arr+i*m+j);
+        }
+        *a[i]=*(arr+i*m);
+    }
+    int sum=0;
+    cout<<endl;
+    cout<<"Array:"<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            sum+=*(arr+i*m+j);
+            cout<<*(arr+i*m+j)<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"Sum : "<<sum<<endl;
+    cout<<"Array of pointers:"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    cout<<endl;
+    cout<<arr<<endl;
+    delete [] a;
+    delete [] arr;
+    return 0;
+}
--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -1,2 +1,26 @@
-package LinkedList;public class LinkedList {
+package LinkedList;
+import java.util.*;
+
+public class LinkedList {
+    static int count(Node head){
+        int count=0;
+        while(head!=null){
+            if(head.data==1){
+                count++;
+            }
+            head=head.next;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Node a=new Node(1);
+        Node b=new Node(2);
+        Node c=new Node(3);
+        a.next=b;
+        b.next=c;
+        c.next=null;
+        Node head=a;
+        int q=count(head);
+        System.out.println(q);
+    }
 }
