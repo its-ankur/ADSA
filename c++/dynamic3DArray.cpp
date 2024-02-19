20496e789346c117faf5c6e51c03287bb2208fce
its-ankur <ankuratul2007@gmail.com>
2024-02-19 12:02:17 +0530

    some improvements

--- c++/dynamic3DArray.cpp
+++ c++/dynamic3DArray.cpp
@@ -1,14 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n,m;
-    cin>>n>>m;
+    int n,m,o;
+    cin>>n>>m>>o;
     int ***a=new int**[n];
     for(int i=0;i<n;i++){
         a[i]=new int*[m];
         for(int j=0;j<m;j++){
-            a[i][j]=new int;
-            cin>>*a[i][j];
+            a[i][j]=new int[o];
+            for(int k=0;k<o;k++){
+                cin>>a[i][j][k];
+            }
         }
     }
     int sum=0;
@@ -16,13 +18,22 @@ int main(){
     cout<<"Array :"<<endl;
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            sum+=*a[i][j];
-            cout<<*a[i][j]<<" ";
+            for(int k=0;k<o;k++){
+                sum+=a[i][j][k];
+                cout<<a[i][j][k]<<" ";
+            }
+            cout<<endl;
         }
         cout<<endl;
     }
     cout<<"Sum : "<<sum<<endl;
     cout<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            delete[] a[i][j];
+        }
+        delete[] a[i];
+    }
     delete [] a;
     return 0;
 }
