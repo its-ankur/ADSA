6b309c07890ecb548de07e449314625feede36e3
its-ankur <ankuratul2007@gmail.com>
2024-02-21 15:41:37 +0530

    stock span problem

--- src/Stack/stockSpan.java
+++ src/Stack/stockSpan.java
@@ -1,7 +1,30 @@
 package Stack;
-
+import java.util.*;
 public class stockSpan {
+    static void span(int[] a,int n,int[] s){
+        Stack<Integer> st=new Stack<>();
+        st.push(0);
+        s[0]=1;
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && a[st.peek()]<=a[i]){
+                st.pop();
+            }
+            s[i] = (st.isEmpty()) ? (i + 1) : (i - st.peek());
+            st.push(i);
+        }
+    }
     public static void main(String[] args) {
-
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        int[] s=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        span(a,n,s);
+        for(int i=0;i<n;i++){
+            System.out.print(s[i]+" ");
+        }
+        System.out.println();
     }
 }
